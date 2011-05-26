1880b4143a87b75e85d5b8fa26578dcfbf2eb3f6
mblabs <webmaster@mblabs.net>
2011-05-26 06:35:00 -0700

    Edited data-sources/data.shopping_cart.php via GitHub

--- data-sources/data.shopping_cart.php
+++ data-sources/data.shopping_cart.php
@@ -7,20 +7,20 @@
 			return array(
 				'name' => 'Shopping Cart',
 				'author' => array(
-				'name' => 'Andrey Lubinov',
+				'name' => 'Andrey Lubinov, Mario Butera',
 				'website' => false,
-				'email' => 'andrey.lubinov@gmail.com'),
-				'version' => '1.2',
+				'email' => 'andrey.lubinov@gmail.com, webmaster@mblabs.net'),
+				'version' => '1.2.1',
 				'description' => '
 					<p>Output example:</p> 
 					<pre><code>' 
-.htmlentities('<shopping-cart items="2" total="381.99">
-	<item id="8" num="2" sum="82" />
-	<item id="9" num="1" sum="299.99" />
+.htmlentities('<shopping-cart items="2" total="381.99" total-weight="150.20">
+	<item id="8" num="2" sum="82" weight="20"/>
+	<item id="9" num="1" sum="299.99" weight="130.20"/>
 </shopping-cart>').
 					'</code></pre>
 					<p>Data Source is also outputs ids of items as <code>$ds-shopping-cart</code> Parameter Output for filtering in another Data Sources</p>',
-				'release-date' => '2010-10-17'
+				'release-date' => '2011-05-26'
 			); 
 		}
 	
@@ -34,19 +34,23 @@
 			}
 			$param_pool['ds-shopping-cart'] = implode(',', array_keys($s));
 			$total = null;
+			$total_weight = null;
 			foreach($s as $key => $value){
 				$item = new XMLElement('item', null, array(
 					'id' => $key,
 					'num' => $value['num'],
-					'sum' => $value['sum'])
+					'sum' => $value['sum'],
+					'weight' => $value['weight'])
 				);
 				$xml->appendChild($item);
 				$total = $total + $value['sum'];
+				$total_weight = $total_weight + $value['weight'];
 			}
 			
 			$xml->setAttributeArray(array(
 				'items' => count($s),
-				'total' => $total)
+				'total' => $total,
+				'total-weight' => $total_weight)
 			);
 			return $xml;
 		}
