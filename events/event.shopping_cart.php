28c91725f41a51430d32c007765fdfeb676a0007
andrrr <andrey.lubinov@gmail.com>
2011-12-22 22:06:43 +0200

    Fixed lost () after Symphony::Database

--- events/event.shopping_cart.php
+++ events/event.shopping_cart.php
@@ -171,7 +171,7 @@
 				B.`id` = '.$this->_id.';';
 			$fieldID = Symphony::Database()->fetchVar('id', 0, $sql);
 			
-			if(!$this->_price = Symphony::Database->fetchVar("value", 0, "
+			if(!$this->_price = Symphony::Database()->fetchVar("value", 0, "
 					SELECT `value` AS `value` 
 					FROM `tbl_entries_data_{$fieldID}` 
 					WHERE `entry_id` = {$this->_id} 
