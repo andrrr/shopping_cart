39b88b722e7aa16d01aecb154053d21cce2ad87c
Andrew Minton <me@andrewminton.co.uk>
2012-06-04 11:46:52 +0100

    Changed DS back to master.. removing weight code.

--- data-sources/data.shopping_cart.php
+++ data-sources/data.shopping_cart.php
@@ -7,22 +7,6 @@
 			return array(
 				'name' => 'Shopping Cart',
 				'author' => array(
-<<<<<<< HEAD
-				'name' => 'Andrey Lubinov, Mario Butera',
-				'website' => false,
-				'email' => 'andrey.lubinov@gmail.com, webmaster@mblabs.net'),
-				'version' => '1.2.1',
-				'description' => '
-					<p>Output example:</p> 
-					<pre><code>' 
-.htmlentities('<shopping-cart items="2" total="381.99" total-weight="150.20">
-	<item id="8" num="2" sum="82" weight="20"/>
-	<item id="9" num="1" sum="299.99" weight="130.20"/>
-</shopping-cart>').
-					'</code></pre>
-					<p>Data Source is also outputs ids of items as <code>$ds-shopping-cart</code> Parameter Output for filtering in another Data Sources</p>',
-				'release-date' => '2011-05-26'
-=======
 				'name' => 'Andrey Lubinov',
 				'website' => false,
 				'email' => 'andrey.lubinov@gmail.com'),
@@ -37,7 +21,6 @@
 					'</code></pre>
 					<p>Data Source is also outputs ids of items as <code>$ds-shopping-cart</code> Parameter Output for filtering in another Data Sources</p>',
 				'release-date' => '2010-10-17'
->>>>>>> 28c91725f41a51430d32c007765fdfeb676a0007
 			); 
 		}
 	
@@ -51,23 +34,19 @@
 			}
 			$param_pool['ds-shopping-cart'] = implode(',', array_keys($s));
 			$total = null;
-			$total_weight = null;
 			foreach($s as $key => $value){
 				$item = new XMLElement('item', null, array(
 					'id' => $key,
 					'num' => $value['num'],
-					'sum' => $value['sum'],
-					'weight' => $value['weight'])
+					'sum' => $value['sum'])
 				);
 				$xml->appendChild($item);
 				$total = $total + $value['sum'];
-				$total_weight = $total_weight + $value['weight'];
 			}
 			
 			$xml->setAttributeArray(array(
 				'items' => count($s),
-				'total' => $total,
-				'total-weight' => $total_weight)
+				'total' => $total)
 			);
 			return $xml;
 		}
